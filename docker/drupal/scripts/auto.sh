8b52af83a44da91b44f74c63992ce9e6874bda49
Jeffery Antoniuk <jeffery.antoniuk@ualberta.ca>
2025-01-17 15:15:49 -0700

    Add debugging to failing action

--- docker/drupal/scripts/auto.sh
+++ docker/drupal/scripts/auto.sh
@@ -17,27 +17,34 @@ function update_leaf_version() {
   # Check Docker Buildx Bake 
   # If TAG and bake var different then update
   # Leverage ability to use multiple file precedence (last take precedence) - https://docs.docker.com/build/bake/reference/
+  echo "Update docker-bake.hcl"
   export FROM=ghcr.io/cwrc/leaf-version-update-helper:local
   LEAF_VERSION=${LEAF_VERSION} docker buildx bake -f docker-bake.hcl -f docker-bake-leaf-version-override.json leaf-version-update-helper --set "drupal.tags=${FROM}"
   id=$(docker create "${FROM}")
+  echo "Container ${id}"
   docker cp $id:/tmp/docker-bake-leaf-version-override.json . 
+  head docker-bake-leaf-version-override.json
   docker rm -v $id
 
   # update Drupal core.extension.yml
+  echo "Update core.extension.yml"
   wget --output-document docker/drupal/rootfs/var/www/drupal/config/sync/core.extension.yml https://gitlab.com/calincs/cwrc/leaf/leaf-base-i8/-/raw/${LEAF_VERSION}/docker/drupal/rootfs/var/www/drupal/config/sync/core.extension.yml
 
   export FROM=ghcr.io/cwrc/drupal-core-extension-helper:local
   docker buildx bake drupal-core-extension-helper --set "drupal.tags=${FROM}"
   id=$(docker create "${FROM}")
+  echo "Container ${id}"
   docker cp $id:/tmp/core.extension.yml docker/drupal/rootfs/var/www/drupal/config/sync/core.extension.yml
   docker rm -v $id
 
   # Update Drupal composer.json and composer.lock
+  echo "Update composer.json & composer.lock"
   wget --output-document docker/drupal/rootfs/var/www/drupal/composer.json https://gitlab.com/calincs/cwrc/leaf/leaf-base-i8/-/raw/${LEAF_VERSION}/docker/drupal/rootfs/var/www/drupal/composer.json
   wget --output-document docker/drupal/rootfs/var/www/drupal/composer.lock https://gitlab.com/calincs/cwrc/leaf/leaf-base-i8/-/raw/${LEAF_VERSION}/docker/drupal/rootfs/var/www/drupal/composer.lock
 
   docker buildx bake -f docker-bake.hcl -f docker-bake-leaf-version-override.json drupal-composer-helper --set "drupal.tags=ghcr.io/cwrc/drupal:local"
   id=$(docker create "ghcr.io/cwrc/drupal:local")
+  echo "Container ${id}"
   docker cp $id:/var/www/drupal/composer.json docker/drupal/rootfs/var/www/drupal/
   docker cp $id:/var/www/drupal/composer.lock docker/drupal/rootfs/var/www/drupal/
   docker rm -v $id
@@ -79,6 +86,6 @@ else
   git checkout -b "leaf_update_${GIT_BRANCH}"
   update_leaf_version_test ${LEAF_VERSION}
   update_leaf_version ${LEAF_VERSION}
-  git commit -a -m "Bump LEAF version from ${LEAF_VERSION_LOCAL} to ${LEAF_VERSION}" && git push origin ${GIT_BRANCH}
+  # git commit -a -m "Bump LEAF version from ${LEAF_VERSION_LOCAL} to ${LEAF_VERSION}" && git push origin ${GIT_BRANCH}
 fi
 
