64fbcb97c6b8a4fcb08f3b254b13a7c36648486b
Jeffery Antoniuk <jeffery.antoniuk@ualberta.ca>
2025-02-18 15:25:18 -0700

    Tweak manual LEAF version update GitHub action.

--- docker/drupal/scripts/manual_leaf_update_helper.sh
+++ docker/drupal/scripts/manual_leaf_update_helper.sh
@@ -27,7 +27,8 @@ function get_local_leaf_version() {
 #   Leverage ability to use multiple file precedence (last take precedence) - https://docs.docker.com/build/bake/reference/
 function update_leaf_version() {
     local LEAF_VERSION=$1
-    python ./docker/drupal/scripts/leaf_version_bake.py \
+
+    python3 ./docker/drupal/scripts/leaf_version_bake.py \
       --src ./docker-bake-leaf-version-override.json \
       --value "${LEAF_VERSION}"
 }
@@ -35,21 +36,23 @@ function update_leaf_version() {
 # update Drupal core.extension.yml
 function update_core_extension() {
     local LEAF_VERSION=$1
+
     wget --output-document docker/drupal/rootfs/var/www/drupal/config/sync/core.extension.yml \
         https://gitlab.com/calincs/cwrc/leaf/leaf-base-i8/-/raw/${LEAF_VERSION}/docker/drupal/rootfs/var/www/drupal/config/sync/core.extension.yml
 
-    python ./docker/drupal/scripts/merge_cwrc_customizations_drupal_core_extensions_yaml.py \
+    python3 ./docker/drupal/scripts/merge_cwrc_customizations_drupal_core_extensions_yaml.py \
         --input ./docker/drupal/scripts/core.extension_cwrc_customizations_template.yml \
         --input ./docker/drupal/rootfs/var/www/drupal/config/sync/core.extension.yml  \
         --output /tmp/core.extension.yml
-  
-  mv /tmp/core.extension.yml ./docker/drupal/rootfs/var/www/drupal/config/sync/core.extension.yml
+
+    mv /tmp/core.extension.yml ./docker/drupal/rootfs/var/www/drupal/config/sync/core.extension.yml
 }
 
 
 # Update Drupal composer.json and composer.lock
 function update_composer_json_lock() {
     local LEAF_VERSION=$1
+
     wget --output-document ./docker/drupal/rootfs/var/www/drupal/composer.json \
         https://gitlab.com/calincs/cwrc/leaf/leaf-base-i8/-/raw/${LEAF_VERSION}/docker/drupal/rootfs/var/www/drupal/composer.json
     wget --output-document ./docker/drupal/rootfs/var/www/drupal/composer.lock \
@@ -57,13 +60,20 @@ function update_composer_json_lock() {
 
     bash ./docker/drupal/scripts/merge_cwrc_customizations.sh && \
     echo "Updating composer.json with drupal/getjwtonlogin and mjordan/islandora_bagger_integration" && \
-    composer require -d ./docker/drupal/rootfs/var/www/drupal 'drupal/getjwtonlogin:^2.0' 'mjordan/islandora_bagger_integration' && \
-    composer install -d ./docker/drupal/rootfs/var/www/drupal
+    composer require -d ./docker/drupal/rootfs/var/www/drupal \
+        --no-scripts --no-plugins --no-install --no-audit --no-interaction \
+        'drupal/getjwtonlogin:^2.0' 'mjordan/islandora_bagger_integration'
 }
 
 #################################################################
 # Main
 #################################################################
+
+if ! command -v python3 &> /dev/null
+then
+    echo "Python3 is not installed"
+    exit 1
+fi
 
 BRANCH=$(git branch --show-current)
 if [ "${BRANCH}" != 'main' ]; then
@@ -92,7 +102,7 @@ else
     GIT_BRANCH="leaf_update_${LEAF_VERSION_REMOTE}"
     
     echo "Create branch ${GIT_BRANCH}"
-    #git checkout -b "${GIT_BRANCH}"
+    git checkout -b "${GIT_BRANCH}"
     
     echo "Update LEAF version"
     update_leaf_version ${LEAF_VERSION_REMOTE}
@@ -104,7 +114,7 @@ else
     COMPOSER_JSON_PATH=./docker/drupal/rootfs/var/www/drupal/composer.json \
         update_composer_json_lock ${LEAF_VERSION_REMOTE}
 
-    git diff 
+    # git --no-pager diff
 
     echo "Commit and push changes to ${GIT_BRANCH}"
     #git commit -a -m "Bump LEAF version from ${LEAF_VERSION_LOCAL} to ${LEAF_VERSION_REMOTE}" && git push origin ${GIT_BRANCH}
